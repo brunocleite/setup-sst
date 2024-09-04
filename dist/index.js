39f0a14e01d80f5047f8f0c11cb4e78507451d43
Bruno Leite <brunocleite@gmail.com>
2024-09-04 17:23:23 -0300

    🎉 feat: update version to 0.0.17 in package.json and package-lock.json
    ✨ feat: add prerelease script for easier versioning and bundling

--- dist/index.js
+++ dist/index.js
@@ -63994,7 +63994,7 @@ async function run() {
         const packageLockPath = core.getInput('package-lock') || './package-lock.json';
         const sstConfigPath = core.getInput('sst-config') || './sst.config.ts';
         const packageLock = JSON.parse(fs.readFileSync(packageLockPath, 'utf-8'));
-        const sstVersion = packageLock['node_modules/sst'].version;
+        const sstVersion = packageLock['node_modules/sst']?.version;
         if (!sstVersion) {
             core.setFailed('SST version could not be parsed');
             return;
